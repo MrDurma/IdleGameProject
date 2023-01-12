f3d2165b1ac023ff89b3510dac3eb92c2b6b859e
MrDurma <durmicanes98@gmail.com>
2023-01-12 14:56:59 +0100

    Implement new feature

---
+++ static/countdown.js
@@ -0,0 +1,29 @@
+// TODO: For now timer works only for one building, implement timer so it works for each busy building.
+function countdown() {
+    let hours, minutes, seconds;
+    let endTime = document.getElementById("end_time").value;
+    let end = new Date();
+    let endHours = endTime.split(':')[0];
+    let endMinutes = endTime.split(':')[1];
+    let endSeconds = endTime.split(':')[2];
+
+    // Calling subtraction function every 1000ms (1 second)
+    setInterval(subtraction, 1000);
+  
+    function subtraction() {
+      document.getElementById("test1").innerHTML = endHours + ":" + endMinutes + ":" + endSeconds;
+      endSeconds--;
+      if (endSeconds <= 0 ) {
+        if (endHours > 0 && endMinutes == 0){
+          endHours--;
+          endMinutes == 59;
+        }
+        else if(endHours == 0 && endMinutes == 0){
+          return;
+        }
+        endSeconds = 59;
+        endMinutes--;
+      }
+    }
+  }
+  
